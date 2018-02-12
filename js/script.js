97c6538e0c747924050caf6f0acd2f7b15cdf0a9
Justyna Piłat <justa0904@gmail.com>
2018-02-12 15:49:35 +0100

    Modify script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,52 @@ $('.input-border').focusin(function() {
 $('.input-border').focusout(function() {
 	$(this).removeClass('active');
 });
+
+
+// Password validation
+
+function equalPasswords() {
+	if($('#password').val() === $('#confirmPassword').val()) {
+		$('.equal-passwords-p').fadeOut(500);
+		return true;
+	} else {		
+		$('.equal-passwords-p').fadeIn(500);
+		return false;
+	}
+}
+$('#terms').click(function() {
+	if($('#password').val() === $('#confirmPassword').val()) {
+		$('.equal-passwords-p').fadeOut(500);
+		return true;
+	} else {		
+		$('.equal-passwords-p').fadeIn(500);
+		$('#password').focus();
+		return false;
+	}
+	
+});
+	
+	
+		
+	//Phone number validation
+
+
+function phonenumber() {
+  	var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$/;
+  	if($('#phone').val() == "") {
+  		$('.valid-number-p').fadeOut(500);
+	}else {
+		if($('#phone').val().match(phoneno)){
+	     	$('.valid-number-p').fadeOut(500);
+		   	return true;
+		}else {
+		   	$('.valid-number-p').fadeIn(500);
+		   	$('#phone').focus();
+		   	return false;
+	    }
+	}
+}
+  
+
+$('#recover').click(phonenumber);
+
